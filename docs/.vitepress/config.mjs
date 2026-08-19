733afb24dedcec3c95ebd28df491474344282f27
Squiggel <208060600+Squiggel@users.noreply.github.com>
2026-08-20 00:32:37 +0100

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vitepress'
+import wikilinks from 'markdown-it-wikilinks'
 
 export default defineConfig({
   title: "My Blog",
   description: "A VitePress Site",
   themeConfig: {
-    docFooter: {
-      prev: false,
-      next: false
-    },
-    // Change outline to an object to set the custom label
-    outline: {
-      level: 'deep',
-      label: 'Contents'
+    docFooter: { prev: false, next: false },
+    outline: { level: 'deep', label: 'Contents' }
+  },
+  
+  // Add this new markdown block
+  markdown: {
+    config: (md) => {
+      // Configured to use the "|" as the alias divider
+      md.use(wikilinks({ makeAllLinksAbsolute: true }))
     }
   }
 })
